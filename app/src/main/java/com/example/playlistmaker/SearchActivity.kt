ffe1825818fede3ff5db4e5313c5320f8fbc17a1
onecoffeeplz <110420215+onecoffeeplz@users.noreply.github.com>
2024-10-07 14:25:53 +0300

    add clear button functionality to search layout

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,6 +1,12 @@
 package com.example.playlistmaker
 
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.example.playlistmaker.databinding.ActivitySearchBinding
 
@@ -8,7 +14,8 @@ class SearchActivity : AppCompatActivity() {
 
     private var _binding: ActivitySearchBinding? = null
     private val binding
-        get() = _binding ?: throw IllegalStateException("Binding for ActivitySearchBinding must not be null!")
+        get() = _binding
+            ?: throw IllegalStateException("Binding for ActivitySearchBinding must not be null!")
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,5 +26,35 @@ class SearchActivity : AppCompatActivity() {
         binding.searchToolbar.setNavigationOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
+
+        binding.clearButton.setOnClickListener {
+            binding.searchbar.text.clear()
+            hideKeyboardAndCursor()
+        }
+
+        val textWatcher = object : TextWatcher {
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.isNullOrEmpty()) {
+                    binding.clearButton.visibility = View.GONE
+                } else {
+                    val input = s.toString()
+                    binding.clearButton.visibility = View.VISIBLE
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        }
+
+        binding.searchbar.addTextChangedListener(textWatcher)
+    }
+
+    private fun hideKeyboardAndCursor() {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        val view: View? = currentFocus
+        if (view is EditText) view.clearFocus()
+        if (view != null) imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
