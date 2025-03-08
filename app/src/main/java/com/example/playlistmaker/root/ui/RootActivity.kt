d2996384b5a0d7e847cade48f05339aef71a34a4
onecoffeeplz <110420215+onecoffeeplz@users.noreply.github.com>
2025-03-08 15:15:06 +0300

    move media screen to fragment & set it as default view

--- app/src/main/java/com/example/playlistmaker/root/ui/RootActivity.kt
+++ app/src/main/java/com/example/playlistmaker/root/ui/RootActivity.kt
@@ -2,7 +2,10 @@ package com.example.playlistmaker.root.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
+import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.ActivityRootBinding
+import com.example.playlistmaker.media.ui.MediaFragment
 
 class RootActivity : AppCompatActivity() {
 
@@ -15,5 +18,11 @@ class RootActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         _binding = ActivityRootBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                this.add(R.id.rootFragmentContainerView, MediaFragment())
+            }
+        }
     }
 }
