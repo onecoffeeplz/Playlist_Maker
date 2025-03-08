8bdbcb5ffd1b969f2ff6af9bcd728fa345461394
onecoffeeplz <110420215+onecoffeeplz@users.noreply.github.com>
2025-03-08 12:43:33 +0300

    move search screen to fragment

--- app/src/main/java/com/example/playlistmaker/main/ui/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/main/ui/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.playlistmaker.media.ui.MediaActivity
-import com.example.playlistmaker.search.ui.SearchActivity
 import com.example.playlistmaker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,10 +16,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.mainSearchButton.setOnClickListener {
-            startActivity(Intent(this, SearchActivity::class.java))
-        }
 
         binding.mainMediaButton.setOnClickListener {
             startActivity(Intent(this, MediaActivity::class.java))
