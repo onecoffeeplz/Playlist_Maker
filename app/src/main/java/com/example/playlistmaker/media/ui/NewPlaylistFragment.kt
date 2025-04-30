d285d721d03e50e328c1efb28bc0ca6179ce34cd
onecoffeeplz <110420215+onecoffeeplz@users.noreply.github.com>
2025-04-30 13:55:19 +0300

    add playlist creation layout

---
+++ app/src/main/java/com/example/playlistmaker/media/ui/NewPlaylistFragment.kt
@@ -0,0 +1,37 @@
+package com.example.playlistmaker.media.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.playlistmaker.databinding.FragmentNewPlaylistBinding
+
+class NewPlaylistFragment : Fragment() {
+
+    private var _binding: FragmentNewPlaylistBinding? = null
+    private val binding
+        get() = _binding
+            ?: throw IllegalStateException("Binding for FragmentNewPlaylistBinding must not be null!")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentNewPlaylistBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    companion object {
+        fun newInstance() = PlaylistsFragment()
+    }
+}
