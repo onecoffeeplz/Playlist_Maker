a63c391de81c2f5ef7b7c1a4e13502ef66bc603e
onecoffeeplz <110420215+onecoffeeplz@users.noreply.github.com>
2025-05-01 14:49:05 +0300

    add playlist cover image picker & back press handler

--- app/src/main/java/com/example/playlistmaker/media/ui/NewPlaylistFragment.kt
+++ app/src/main/java/com/example/playlistmaker/media/ui/NewPlaylistFragment.kt
@@ -1,14 +1,20 @@
 package com.example.playlistmaker.media.ui
 
+import android.net.Uri
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.FragmentNewPlaylistBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class NewPlaylistFragment : Fragment() {
 
@@ -16,6 +22,8 @@ class NewPlaylistFragment : Fragment() {
     private val binding
         get() = _binding
             ?: throw IllegalStateException("Binding for FragmentNewPlaylistBinding must not be null!")
+
+    private var uri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,7 +42,7 @@ class NewPlaylistFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.newPlaylistToolbar.setNavigationOnClickListener {
-            requireActivity().supportFragmentManager.popBackStack()
+            handleBackPressed()
         }
 
         val textWatcher = object : TextWatcher {
@@ -46,6 +54,50 @@ class NewPlaylistFragment : Fragment() {
         }
         binding.playlistName.addTextChangedListener(textWatcher)
 
+        val pickMedia =
+            registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { selectedUri ->
+                if (selectedUri != null) {
+                    uri = selectedUri
+                    binding.playlistCover.setImageURI(uri)
+                }
+            }
+
+        binding.playlistCover.setOnClickListener {
+            pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
+
+        requireActivity().onBackPressedDispatcher.addCallback(
+            viewLifecycleOwner,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    handleBackPressed()
+                }
+            })
+    }
+
+    private fun showExitConfirmationDialog() {
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle(R.string.playlist_dialog_title)
+            .setMessage(R.string.playlist_dialog_body)
+            .setNeutralButton(R.string.playlist_dialog_cancel) { dialog, _ ->
+                dialog.dismiss()
+            }
+            .setPositiveButton(R.string.playlist_dialog_finish) { dialog, _ ->
+                dialog.dismiss()
+                findNavController().navigateUp()
+            }
+            .show()
+    }
+
+    private fun handleBackPressed() {
+        if (binding.playlistName.text.toString()
+                .isNotEmpty() || binding.playlistDescription.text.toString()
+                .isNotEmpty() || uri != null
+        ) {
+            showExitConfirmationDialog()
+        } else {
+            findNavController().navigateUp()
+        }
     }
 
     override fun onDestroyView() {
